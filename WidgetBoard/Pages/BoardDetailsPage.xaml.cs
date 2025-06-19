952d889daa6ebd16c11d3118e67a6c6eeb5ebd5a
Abdulmalik Muhammad <abdulmalik.m.m@nuprc.gov.ng>
2025-06-19 19:55:44 +0100

    User Interface Essentials

---
+++ WidgetBoard/Pages/BoardDetailsPage.xaml.cs
@@ -0,0 +1,12 @@
+using WidgetBoard.ViewModels;
+
+namespace WidgetBoard.Pages;
+
+public partial class BoardDetailsPage : ContentPage
+{
+	public BoardDetailsPage(BoardDetailsPageViewModel boardDetailsPageViewModel)
+	{
+		InitializeComponent();
+		BindingContext = boardDetailsPageViewModel;
+	}
+}
---
+++ WidgetBoard/ViewModels/BoardDetailsPageViewModel.cs
@@ -0,0 +1,59 @@
+﻿using WidgetBoard.Models;
+
+namespace WidgetBoard.ViewModels;
+
+public partial class BoardDetailsPageViewModel : BaseViewModel
+{
+	private string boardName = string.Empty;
+
+	public string BoardName
+	{
+		get => boardName;
+		set
+		{
+			SetProperty(ref boardName, value);
+			SaveCommand.ChangeCanExecute();
+        }
+	}
+
+	private bool isFixed;
+
+	public bool IsFixed
+	{
+		get => isFixed;
+		set => SetProperty(ref isFixed, value);
+	}
+
+	private int numberOfColumns = 3;
+
+	public int NumberOfColumns
+	{
+		get => numberOfColumns;
+		set => SetProperty(ref numberOfColumns, value);
+	}
+
+	private int numberOfRows = 2;
+
+	public int NumberOfRows
+	{
+		get => numberOfRows;
+		set => SetProperty(ref numberOfRows, value);
+	}
+
+	public Command SaveCommand { get; }
+
+    public BoardDetailsPageViewModel()
+    {
+		SaveCommand = new(Save, () => !string.IsNullOrWhiteSpace(BoardName));
+    }
+
+    private void Save()
+	{
+		Board board = new()
+		{
+			Name = BoardName,
+			NumberOfColumns = NumberOfColumns,
+			NumberOfRows = NumberOfRows
+		};
+	}
+}
